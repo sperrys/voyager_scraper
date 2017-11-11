469dcec522a2f20a8b15dfead8280837c353e7a1
Spencer Perry <sperry@Spencers-MacBook-Pro.local>
2017-11-10 19:06:38 -0500

    changed proc

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:voyager_scraper
+web: gunicorn app:app
--- voyager_scraper.py
+++ voyager_scraper.py
@@ -37,6 +37,5 @@ def get_distance():
 
 	return sanitized
 
-
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+	app.run()
