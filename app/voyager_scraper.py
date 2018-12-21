d222aadc683b8d821ff1e53f985937f7ec400b44
Spencer Perry <spencerfarrallperry@gmail.com>
2018-12-21 10:19:58 -0500

    cleanup code for clarity sake

--- app/voyager_scraper.py
+++ app/voyager_scraper.py
@@ -1,14 +1,16 @@
 import requests
 import time
+import os
 
 from contextlib import contextmanager
 from flask import Flask
 from bs4 import BeautifulSoup
 
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ChromeOptions, Chrome
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 
 app = Flask(__name__)
 
@@ -17,8 +19,17 @@ def get_distance():
 
 	URL = "https://voyager.jpl.nasa.gov/mission/status/"
 
+	chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+
+	opts = ChromeOptions()
+	opts.binary_location = chrome_bin
+
+	opts.add_argument('headless')
+	opts.add_argument('--disable-gpu')
+	opts.add_argument('--no-sandbox')
+
 	# Start the WebDriver and load the page
-	wd = webdriver.Chrome()
+	wd = Chrome(executable_path="chromedriver", chrome_options=opts)
 	wd.get(URL)
 
 	# Wait for the dynamically loaded elements to show up
@@ -34,8 +45,9 @@ def get_distance():
 	distance = soup.find(id="voy1_km").get_text()
 	
 	sanitized = distance.split()[0]
+	no_commas = sanitized.replace(",", "")
 
-	return sanitized
+	return no_commas
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
